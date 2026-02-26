31c8fb119f043280ba8767e0c4af38ddf5456e5d
yukio113 <ykoskrg@outlook.jp>
2026-02-26 17:03:46 +0900

    removed export const dynamic

--- app/auth/callback/page.tsx
+++ app/auth/callback/page.tsx
@@ -1,8 +1,6 @@
 import { Suspense } from "react";
 import AuthCallbackClient from "./AuthCallbackClient";
 
-export const dynamic = "force-dynamic";
-
 export default function AuthCallbackPage() {
   return (
     <Suspense
